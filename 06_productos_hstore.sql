04b54103290d77d73f058a9149932f150f77db6c
SayraIbarra <130393897+SayraIbarra@users.noreply.github.com>
2025-11-18 02:43:07 -0600

    Delete 06_productos_hstore.sql

--- 06_productos_hstore.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO productos_jsonb (nombre, especificaciones)
-VALUES 
-  ('Laptop Gaming', '{"marca": "ASUS", "color": "negro", "ram": "16GB", "almacenamiento": "1TB SSD"}'),
-  ('Smartphone', '{"marca": "Samsung", "color": "azul", "almacenamiento": "128GB", "pantalla": "6.1"}'),
-  ('Tablet', '{"marca": "Apple", "color": "plateado", "almacenamiento": "256GB", "conectividad": "WiFi"}'),
-  ('Monitor', '{"marca": "LG", "color": "negro", "tamaño": "27", "resolucion": "4K"}'),
-  ('Teclado', '{"marca": "Logitech", "color": "blanco", "inalambrico": true, "mecanico": false}');
